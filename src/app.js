4b88c3ef4bcb57ef6bd8178e7c0c0d371c5f6f55
Sacha Sedriks <sachased@gmail.com>
2020-07-08 15:10:11 -0700

    refactored: modularizing code by routing bookmarks CRUD and layering code by creating components for any middleware functions.

--- src/app.js
+++ src/app.js
@@ -3,105 +3,22 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const winston = require('winston')
-const { bookmarks } = require('../store')
-const { v4: uuid } = require('uuid')
+const fourOhFourErrorHandler = require('./middleware/fourOhFourErrorHandler')
+const serverErrorHandler = require('./middleware/serverErrorHandler')
 
+const { NODE_ENV } = require('./config')
+const bookmarksRouter = require('./routes/bookmarks.router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production') 
-  ? 'tiny' 
-  : 'dev';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'dev'))
 app.use(helmet())
 app.use(cors())
-app.use(express.json());
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({ filename: 'info.log' })
-  ]
-});
+app.use('/bookmarks', bookmarksRouter)
 
-if (NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-  }));
-}
+app.use(fourOhFourErrorHandler)
+app.use(serverErrorHandler)
 
-app.get('/bookmarks', (req, res) => {
-  res.send(bookmarks)
-})
-
-app.get('/bookmarks/:id', (req, res) => {
-  const { id } = req.params
-
-  const bookmark = bookmarks.find(b => b.id == id)
-  
-  if(!bookmark){
-    logger.error(`Bookmark with id: ${id} not found`)
-    return res.status(404).send('bookmark not found')
-  }
-  res.status(200).json(bookmark)
-})
-
-app.post('/bookmarks', (req, res) =>{
-  const { title, url, description, rating = 1 } = req.body
-
-  if(!title){
-    return res.status(400).send('Invalid! title required.')
-  }
-  if(!url){
-    return res.status(400).send('Invalid! url required.')
-  }
-  if(!description){
-    return res.status(400).send('Invalid! description required.')
-  }
-  const id = uuid();
-
-  const bookmark ={
-    id,
-    title,
-    url,
-    description,
-    rating
-  }
-  bookmarks.push(bookmark)
-  logger.info(`Bookmark with id: ${id} created.`)
-  // res.status(204).end()
-  res
-   .status(201)
-   .location(`http//localhost:8000/bookmarks/${id}`)
-   .json(bookmark)
-})
-
-app.delete('/bookmarks/:id', (req, res) => {
-  const { id } = req.params
-
-  const bookmarkIndex = bookmarks.findIndex(b => b.id == id )
-  if(bookmarkIndex === -1){
-    logger.error(`Bookmark with id: ${id} not found`)
-    return res.status(404).send('Not found!')
-  }
-  bookmarks.splice(bookmarkIndex, 1)
-  logger.info(`Bookmark with id: ${id} deleted`)
-  res.status(204).end()
-})
-
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
-})
 
 module.exports = app
