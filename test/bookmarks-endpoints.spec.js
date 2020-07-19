63fb18dd7a8c2c8a9c1250a74b31cda77b3a5f88
Sacha Sedriks <sachased@gmail.com>
2020-07-19 14:23:24 -0700

    moved test array into it's own fixtures file

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const knex = require('knex');
 const app = require('../src/app');
 const supertest = require('supertest');
+const fixtures = require('./bookmarks.fixtures');
 
 describe.only('Bookmark Endpoints (bookmarks-endpoints-spec.js)', () => {
 
@@ -22,29 +23,7 @@ describe.only('Bookmark Endpoints (bookmarks-endpoints-spec.js)', () => {
 
     context(`Given 'bookmarks' database and table has data`, () => {
 
-        let testBookmarks = [
-            {
-                id: 1,
-                title: 'Bookmark Test One',
-                url: 'https://giphy.com/',
-                description: 'humerous gif animes',
-                rating: 5,
-            },
-            {
-                id: 2,
-                title: 'Bookmark Test Two',
-                url: 'https://medium.com/',
-                description: '',                // intentionally left blank, as not required
-                rating: 4,
-            },
-            {
-                id: 3,
-                title: 'Bookmark Test Three',
-                url: 'http://casts.thinkfullabs.com/',
-                description: 'complementary to curriculum',
-                rating: 3,
-            },
-        ];
+        const testBookmarks = fixtures.makeBookmarksArray();
 
         beforeEach('insert bookmarks', () => {
             return db
