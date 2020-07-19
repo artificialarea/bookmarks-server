0f4d71f6babef8dd8bd896ce2ec389769f9f8a77
Sacha Sedriks <sachased@gmail.com>
2020-07-19 14:30:16 -0700

    refactored tests so each endpoint has a separate describe (with multiple contexts within).

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -4,7 +4,7 @@ const app = require('../src/app');
 const supertest = require('supertest');
 const fixtures = require('./bookmarks.fixtures');
 
-describe.only('Bookmark Endpoints (bookmarks-endpoints-spec.js)', () => {
+describe('Bookmark Endpoints (bookmarks-endpoints-spec.js)', () => {
 
     let db;
 
@@ -21,59 +21,78 @@ describe.only('Bookmark Endpoints (bookmarks-endpoints-spec.js)', () => {
     before('clean the table', () => db('bookmarks').truncate());
     afterEach('cleanup', () => db('bookmarks').truncate());
 
-    context(`Given 'bookmarks' database and table has data`, () => {
+    describe(`GET /bookmarks`, () => {
 
-        const testBookmarks = fixtures.makeBookmarksArray();
+        context(`Given 'bookmarks' database and table has data`, () => {
 
-        beforeEach('insert bookmarks', () => {
-            return db
-                .into('bookmarks')
-                .insert(testBookmarks)
+            const testBookmarks = fixtures.makeBookmarksArray();
+    
+            beforeEach('insert bookmarks', () => {
+                return db
+                    .into('bookmarks')
+                    .insert(testBookmarks)
+            });
+    
+            it(`responds with 200 and all of the bookmarks`, () => {
+                return supertest(app)
+                    .get('/bookmarks')
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)  // via setup.js
+                    .expect(200, testBookmarks)
+            });
         });
 
-        it(`GET /bookmarks responds with 200 and all of the bookmarks`, () => {
-            return supertest(app)
-                .get('/bookmarks')
-                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)  // via setup.js
-                .expect(200, testBookmarks)
-        
+        context(`Given no bookmarks in database`, () => {
+
+            it(`responds with 200 and an empty list`, () => {
+                return supertest(app)
+                    .get(`/bookmarks`)
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                    .expect(200, [])
+            });
         });
-
-        it(`GET /bookmarks/:id responds with 200 and the specified bookmark`, () => {
-            const bookmarkId = 3;
-            const expectedBookmark = testBookmarks[bookmarkId - 1];
-            // console.log(expectedBookmark)
-            return supertest(app)
-                .get(`/bookmarks/${bookmarkId}`)
-                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-                .expect(200, expectedBookmark)
-        });
-
-        it(`GET /bookmarks/:id responds 404 when bookmark doesn't exist`, () => {
-            return supertest(app)
-                .get(`/bookmarks/123`)
-                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-                .expect(404, `Bookmark Not Found`)
-        })
-
+    
     });
 
-    context(`Given no bookmarks in database`, () => {
+    describe(`GET /bookmarks/:id`, () => {
+        
+        context(`Given 'bookmarks' database and table has data`, () => {
 
-        it(`GET /bookmarks responds with 200 and an empty list`, () => {
-            return supertest(app)
-                .get(`/bookmarks`)
-                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-                .expect(200, [])
+            const testBookmarks = fixtures.makeBookmarksArray();
+    
+            beforeEach('insert bookmarks', () => {
+                return db
+                    .into('bookmarks')
+                    .insert(testBookmarks)
+            });
+    
+            it(`responds with 200 and the specified bookmark`, () => {
+                const bookmarkId = 3;
+                const expectedBookmark = testBookmarks[bookmarkId - 1];
+                // console.log(expectedBookmark)
+                return supertest(app)
+                    .get(`/bookmarks/${bookmarkId}`)
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                    .expect(200, expectedBookmark)
+            });
+    
+            it(`responds 404 when bookmark doesn't exist`, () => {
+                return supertest(app)
+                    .get(`/bookmarks/123`)
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                    .expect(404, `Bookmark Not Found`)
+            });
         });
 
-        it(`GET /bookmarks/:id responds 404 when bookmark doesn't exist`, () => {
-            return supertest(app)
-                .get(`/bookmarks/123`)
-                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
-                .expect(404, `Bookmark Not Found`)
-        })
-    })
+        context(`Given no bookmarks in database`, () => {
 
+            it(`responds 404 when bookmark doesn't exist`, () => {
+                return supertest(app)
+                    .get(`/bookmarks/123`)
+                    .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                    .expect(404, `Bookmark Not Found`)
+            });
+        });
+
+    });
 
 });
